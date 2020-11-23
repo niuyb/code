34262173b1d244451308ef7ca7a35409e5c32a34
niuyb <niuyubin@yqzbw.com>
2020-11-23 10:22:00 +0800

    跳台阶

---
+++ 8_跳台阶.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# 作者：Ksar4
+# 日期：2020/11/23 9:09
+# 工具：PyCharm
+# Python版本：3.7.0
+""""""
+
+
+
+
+"""
+一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）。
+"""
+
+"""
+1 ->  1
+4 ->  5
+"""
+
+
+class Solution:
+    def jumpFloor(self, number):
+        if number==1:
+            return 1
+        if number==2:
+            return 2
+        arr=[1,2]
+        for i in range(2,number):
+            temp=arr[i-1]+arr[i-2]
+            arr.append(temp)
+        return arr[number-1]
+
+
+
+
+if __name__ == "__main__":
+
+    s = Solution()
+    print(s.jumpFloor(5))
+
+
