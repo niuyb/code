da9e98210d14a9972972c9067a14b9e01a5199a3
niuyb <niuyubin@yqzbw.com>
2020-12-08 17:23:11 +0800

    快速排序

---
+++ 快速排序.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# 作者：Ksar4
+# 日期：2020/12/8 17:22
+# 工具：PyCharm
+# Python版本：3.7.0
+
+
+
+
+
+
+
+
+def quick_sort(list):
+    less = []
+    pivotList = []
+    more = []
+    # 递归出口
+    if len(list) <= 1:
+        return list
+    else:
+        # 将第一个值做为基准
+        pivot = list[0]
+        for i in list:
+            # 将比急转小的值放到less数列
+            if i < pivot:
+                less.append(i)
+            # 将比基准打的值放到more数列
+            elif i > pivot:
+                more.append(i)
+            # 将和基准相同的值保存在基准数列
+            else:
+                pivotList.append(i)
+        # 对less数列和more数列继续进行排序
+        less = quick_sort(less)
+        more = quick_sort(more)
+        return less + pivotList + more
