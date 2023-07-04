4b4f9b3aa79193367db01c286d28c55faa509b4c
Ksar4 <niuybksar4@163.com>
2023-07-04 21:04:23 +0800

    array to tree

---
+++ golang_leetcode75/数组生成二叉树.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 中间节点生成树
+func arrayToTree(array []int) *TreeNode {
+	// index := -1
+	return buildTree(array, 0, len(array)-1)
+}
+func buildTree(nums []int, start int, end int) *TreeNode {
+	// 如果 start > end ，说明用于生产二叉树的数组区间为空，
+	// 则对应的二叉树为空，返回 None
+	if start > end {
+		return nil
+	}
+	// 根节点就是区间 [start, end] 中间的数，这样就能保证根节点是高度平衡的，
+	// 因为左右子树的结点数相差不超过 1
+	mid := (start + end) / 2
+	return &TreeNode{
+		Val: nums[mid],
+		// 递归生成左右子树
+		Left:  buildTree(nums, start, mid-1),
+		Right: buildTree(nums, mid+1, end),
+	}
+}
+func getTreeHeight(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return int(1 + math.Max(float64(getTreeHeight(root.Left)), float64(getTreeHeight(root.Right))))
+}
+func writeArray(root *TreeNode, row, column, treeHeight int, resArray [][]string) {
+	if root == nil {
+		return
+	}
+	resArray[row][column] = strconv.Itoa(root.Val)
+	currentHeight := (row + 1) / 2 // 当前高度
+	if currentHeight == treeHeight {
+		return // 下面没有子节点了
+	}
+	gap := treeHeight - currentHeight - 1 // 到左/右儿子的距离
+	// 填充左儿子
+	if root.Left != nil {
+		// 先写树结构符号
+		resArray[row+1][column-gap] = "/"
+		// 再写左儿子
+		writeArray(root.Left, row+2, column-gap*2, treeHeight, resArray)
+	}
+	// 填充右儿子
+	if root.Right != nil {
+		resArray[row+1][column+gap] = "\\"
+		writeArray(root.Right, row+2, column+gap*2, treeHeight, resArray)
+	}
+}
+func printAvlTree(root *TreeNode) {
+	height := getTreeHeight(root)
+	fmt.Printf("height: %v\n", height)
+	// 总宽度为节点高度 * 2 - 1, 因为还要画树枝符号
+	totalHeight := height*2 - 1
+	// 最大宽度为3 * 2^(n - 1) + 1，公式如下：
+	/**
+	   父亲节点占1, 两个孩子空间各占1, 连接线各占1, 每个父子关系共占5, 每个关系之间空1, 最左最右各空1
+	  第2行： 5 + 2 （1个父子结构占位+左右两个空格分割）
+	  第3行：2 * 5 + (1 + 2) （2个父子结构占位+1个相邻父子结构间空格+左右两个空格分割）
+	  第4行：4 * 5 + (3 + 2) （4个父子结构占位+3个相邻父子结构间空格+左右两个空格分割）
+	  第5行：8 * 5 + (7 + 2)
+	  第n行：5 * 2 ^ (n - 2) + (2 ^ (n - 2) - 1) + 2 = 6 * 2 ^ (n-2) + 1 = 3 * 2 ^ (n - 1) + 1
+	*/
+	var totalWidth int
+	if height == 0 {
+		totalWidth = 1
+	} else {
+		totalWidth = (2<<(height-2))*3 + 1
+	}
+
+	// 创建数组
+	printArray := make([][]string, totalHeight)
+	for i := range printArray {
+		printArray[i] = make([]string, totalWidth)
+		for j := range printArray[i] {
+			printArray[i][j] = " "
+		}
+	}
+
+	// 计算打印数组
+	writeArray(root, 0, totalWidth/2, height, printArray)
+
+	// 打印
+	for i := range printArray {
+		var res string
+		for j := range printArray[i] {
+			res = res + printArray[i][j]
+		}
+		fmt.Println(res)
+	}
+}
+
+// 广度遍历数组生成树
+func bfsArrayToTree(tree_list []int) *TreeNode {
+	// 二叉树元素栈
+	var tree_stack []*TreeNode
+	// root 节点
+	root := &TreeNode{
+		Val: tree_list[0],
+	}
+	// 节点数组去除根结点
+	tree_list = tree_list[1:]
+	// 跟节点入栈
+	tree_stack = append(tree_stack, root)
+	// 循环建立二叉树
+	bfsBuildTree(tree_list, tree_stack)
+	return root
+}
+
+func bfsBuildTree(tree_list []int, tree_stack []*TreeNode) {
+	// 无栈可出时退出，说明遍历结束
+	for len(tree_stack) > 0 {
+		// 深copy 栈 用于不受清空栈操作影响
+		temp_tree_stack := make([]*TreeNode, len(tree_stack))
+		copy(temp_tree_stack, tree_stack)
+		// 清空栈 不影响后续元素获取子节点
+		tree_stack = tree_stack[0:0]
+		// 遍历栈 出栈
+		for _, node := range temp_tree_stack {
+			// 出栈 链接左右子节点
+			if len(tree_list) > 0 {
+				node.Left = &TreeNode{
+					Val: tree_list[0],
+				}
+				node.Right = &TreeNode{
+					Val: tree_list[1],
+				}
+				// 左右子节点再次入栈
+				tree_stack = append(tree_stack, node.Left)
+				tree_stack = append(tree_stack, node.Right)
+				// 二叉树左右子节点 只有2个
+				tree_list = tree_list[2:]
+			}
+		}
+
+	}
+}
+
+func main() {
+
+	// inf := math.Inf(-1)
+	// tree_list := []float64{3, 9, 20, inf, inf, 15, 7}
+	tree_list := []int{3, 9, 20, 0, 0, 15, 7}
+
+	root := bfsArrayToTree(tree_list)
+
+	// root := arrayToTree(tree_list)
+	printAvlTree(root)
+
+}
